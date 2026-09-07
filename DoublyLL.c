1bb7c1143f48b23d979362003f0217e733f92b62
Pamod Thiwanka <pamod.thiwanka@gmail.com>
2026-09-07 20:12:07 +0530

    Add files via upload

---
+++ DoublyLL.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+  struct Node
+ {
+     int data;
+     struct Node*next;
+     struct Node*prev;
+ };
+ struct Node*head=NULL;
+ struct Node*tail=NULL;
+ struct Node*temp=NULL;
+ int count=0;
+ void CreateNode(int value)
+ {
+     struct Node*newnode;
+     newnode=malloc(sizeof(struct Node));
+     newnode->data=value;
+     newnode->next=NULL;
+     newnode->prev=NULL;
+     if(head==NULL)
+     {
+         head=tail=newnode;
+     }
+     else
+     {
+         tail->next=newnode;
+         newnode->prev=tail;
+         tail=newnode;
+     }
+ }
+int main(){
+    int choice=1;
+    while(choice!=0)
+    {
+    int data;
+    printf("enter the data:");
+    scanf("%d",&data);
+    CreateNode(data);
+    printf("do you want to continue?(1/0):");
+    scanf("%d",&choice);
+    }
+    PrintList();
+    return 0;
+
+}
+void PrintList()
+{
+    temp=head;
+    printf("Linked List:");
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+        count++;
+    }
+    printf("\n");
+    printf("%d\n",count);
+}
